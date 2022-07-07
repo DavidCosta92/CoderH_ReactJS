7066dab6bf5fa87eedc5c7469ec4b8355815db57
David Costa <davidcst2991@gmail.com>
2022-07-07 20:53:21 -0300

    segundo commit, colocando proyecto a cero

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mi primer Ecomerce con React</h1>
+      <p>Nombre pendiente....</p>
+      
     </div>
   );
 }
 
 export default App;
-
-
-function probando(){
-  console.log("estoy probando una funcion?")
-}

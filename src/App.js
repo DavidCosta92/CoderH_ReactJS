b8b61783cce84eca1d514ba7c8df92796d9ee2a1
David Costa <davidcst2991@gmail.com>
2022-07-07 21:05:55 -0300

    nombre de tienda

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Mi primer Ecomerce con React</h1>
-      <p>Nombre pendiente....</p>
+      <h3>Mi primer Ecomerce con React</h3>
+      <p>Soy una cafeteria</p>
+      <p>y mi nombre es</p>
+      <h1>Taibe</h1>
       
+
     </div>
   );
 }
